60d831424d8812519420859d0b5680c54b90cc83
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2022-06-09 19:20:04 +0200

    carve out online loop

--- main.py
+++ main.py
@@ -3,12 +3,11 @@
 For more information, see the `README.md` file and follow the steps from the
 "Running the project" section.
 
-buffer len: 1s
-bandpass between 1Hz and 30Hz
-
 """
 # %%
 # Imports
+import time
+
 import mne
 import numpy as np
 from pylsl import StreamInlet, resolve_stream
@@ -22,7 +21,7 @@ ch_names = [
     "FT10", "T7", "C3", "Cz", "C4", "T8", "CP5", "CP1", "CP2", "CP6", "TP9", "P7",
     "P3", "Pz", "P4", "P8", "TP10", "O1", "Oz", "O2"]
 # fmt: on
-sfreq = 250.0
+sfreq = 250
 ch_types = ["eeg"] * len(ch_names) + ["misc"]
 ch_names += ["marker"]  # there is an additional marker channel inserted by LSL
 
@@ -32,31 +31,88 @@ info = mne.create_info(ch_names, sfreq, ch_types)
 # caps that we use in the lab
 info.set_montage("easycap-M1")
 
+# %%
+# EEG Settings
+# how many samples to pull: sfreq = 1 second (Hz)
+n_samples = sfreq
+
+# how many seconds to wait until data is there
+max_wait = (n_samples / sfreq) * 5
+
+# default "events" that we will use for each incoming data
+events = np.expand_dims(np.array([0, 1, 1]), axis=0)
+
+# Later on, we will only be interested in EEG channels
+picks = mne.pick_types(info, eeg=True)
+info = mne.pick_info(info, picks)
+
+# The frequencies we want to average over (alpha power)
+fmin = 8
+fmax = 12
+
+# needed for psd_welch
+n_fft = 128
+assert n_fft < n_samples, "n_fft must be <= n_samples"
+
+# We will subtract power from posterior and frontal channels
+posterior_chs = []
+frontal_chs = []
+picks_posterior = mne.pick_channels(info, posterior_chs)
+picks_frontal = mne.pick_channels(info, frontal_chs)
+
+# %%
+# Psychopy window settings
+pass
 
 # %%
 # Connect to the BrainVision Recorder RDA (Remote Data Access)
 # (needs to be switched on in BrainVision Recorder)
 streams = resolve_stream("type", "EEG")
-assert len(streams) == 1
+assert len(streams) == 1, f"expected one stream, but found: {len(streams)}"
 inlet = StreamInlet(streams[0])
 
-# clears all present not-yet-pulled samples from the buffer
+# %%
+# Begin the loop
+
+# first clear all present not-yet-pulled samples from the buffer
 inlet.flush()
 
-# pull a chunk of data: `chunk` is a list of lists, each list corresponding to a sample
-# with an associated timestamp. Each list is of length n_channels + 1, in order as
-# defined in BrainVision Recorder workspace setup. The last channel ("+1") is a
-# marker channel that can be ignored.
-chunk, timestamps = inlet.pull_chunk(timeout=0.0, max_samples=1000)
+# end this by interrupting the process (ctrl+c)
+while True:
 
-# Then convert to mne Epochs
-info = 1
-n_samples = 1000
-wait_time = n_samples * 5.0 / info["sfreq"]
-samples, _ = inlet.pull_chunk(max_samples=n_samples, timeout=wait_time)
-data = np.vstack(samples).T
+    # measure time that this iteration takes
+    tstart_ns = time.perf_counter_ns()
 
-events = np.expand_dims(np.array([0, 1, 1]), axis=0)
-picks = mne.io.pick._picks_to_idx(info, "eeg", "all", exclude=())
-info = mne.io.pick.pick_info(info, picks)
-epochs = mne.EpochsArray(data[picks][np.newaxis], info, events)
+    # pull a chunk of data: `chunk` is a list of lists, each list corresponding to a
+    # sample with an associated timestamp. Each list is of length n_channels + 1, in
+    # order as defined in BrainVision Recorder workspace setup. The last channel ("+1")
+    # is a marker channel that can be ignored.
+    chunk, timestamps = inlet.pull_chunk(timeout=max_wait, max_samples=n_samples)
+
+    # Then convert to mne Epochs
+    data = np.vstack(chunk).T
+    epochs = mne.EpochsArray(data[picks][np.newaxis], info, events, verbose=0)
+
+    # Calculate power spectrum, ´psds´ is of shape (n_epochs, n_channels, n_freqs)
+    psds, freqs = mne.time_frequency.psd_welch(
+        epochs, fmin, fmax, n_fft=n_fft, average="mean", verbose=0
+    )
+
+    # Separatetly average power for posterior and frontal channels
+    power_posterior = np.mean(psds[0, picks_posterior, :])
+    power_frontal = np.mean(psds[0, picks_frontal, :])
+
+    # Take the sign of the difference: this is our "color switch"
+    switch = np.sign(power_posterior - power_frontal)
+    print(f"State of switch: {switch}")
+
+    # If this iteration took too long, we need to flush the buffer,
+    # so that we always have fresh data
+    tstop_ns = time.perf_counter_ns()
+    titer_s = tstop_ns - tstart_ns * 1e-9
+    if titer_s > (n_samples / sfreq):
+        print(f"\n    >>>> Iteration took {titer_s} s ... clearing buffer.\n")
+        inlet.flush()
+
+    # Start next iteration
+    pass
